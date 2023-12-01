b1c1b4b725ea13fd60cf835eb714904f269496c2
Goetz Markgraf <goeetz.markgraf@codecentric.de>
2023-12-01 14:31:09 +0100

    day 1

---
+++ src/lib.rs
@@ -0,0 +1,42 @@
+use std::fmt::Debug;
+
+pub fn solution<I, O>(
+    input_file: &str,
+    parse: fn(Vec<String>) -> I,
+    solve: fn(I) -> O,
+    expected: O,
+) -> bool
+    where
+        O: PartialEq + Debug,
+{
+    let long_file_name = format!("input/{}.txt", input_file);
+    let file_content = match std::fs::read_to_string(&long_file_name) {
+        Ok(content) => content,
+        Err(_) => {
+            println!("Could not read input file {}", &long_file_name);
+            return false;
+        }
+    };
+
+    let strings = file_content
+        .lines()
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
+
+    let data_structure = parse(strings);
+    let result = solve(data_structure);
+
+    if result == expected {
+        println!("{}: Correct Result {:?}", input_file, result);
+        true
+    } else {
+        println!("{}: Expected {:?}, got {:?}", input_file, expected, result);
+        false
+    }
+}
+
+// a function the takes a vector of strings and
+// returns the same vector without changing the strings
+pub fn parse_to_strings(strings: Vec<String>) -> Vec<String> {
+    strings
+}
